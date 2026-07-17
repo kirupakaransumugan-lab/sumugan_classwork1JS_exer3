338a80b7a4a6ec3dc2339c873974719facd76f05
kirupakaransumugan <kirupakaransumugan@gmail.com>
2026-07-17 06:54:54 +0530

    new1

--- classwork1_ex3.js
+++ classwork1_ex3.js
@@ -19,7 +19,7 @@ function addNumbers(x,y) {
     return x+y;
 }
 
-let firstNumber = Number(prompt("Enter the first number:"));
+let firstNumber = Number(prompt("Enter the first number:")); /// prompt for ask inuput from users number for assign that is number input 
 let secondNumber = Number(prompt("Enter the second number:"));
 
 let total = addNumbers(firstNumber, secondNumber);
@@ -28,14 +28,14 @@ console.log("total=", total);
 
 //q4
 
- const area = (a,b) => a*b ;
+ const area = (a,b) => a*b ;   //arrow functin 
 
   console.log(area(10,20));
 
   //q5
 
 
-  let peri = ( l,w) => {
+  let peri = ( l,w) => {     
     return 2*(l+w);
   }
 
@@ -57,12 +57,12 @@ console.log("Volume =", volume);
  //q7
 
  function Circle(radius) {
-    return Math.PI * radius * radius;
+    return Math.PI * radius * radius;     /// matha pi = 3.14 
 }
 
 let radius = Number(prompt("Enter the radius:"));
 
-let area1 = Circle(radius);
+let area1 = Circle(radius);   /// we asked input from user i thioonk we diorectly put input to funtion from get from uer thay why area1 varuble do that job
 
 console.log("Area=", area1);
 
@@ -75,7 +75,8 @@ function Circle1(ra) {
 
 let ra = Number(prompt("Enter the radius:"));
 
-let circum = Circle1(ra);
+let circum = Circle1(ra);     /// we asked input from user input  thioonk we diorectly put input to funtion from get from uer thay why area1 varuble do that job
+
 
 console.log("Circle =", circum);
 
